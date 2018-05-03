16b4f57249644b07c524234c90391cb9786cea41
RuihaoQiu <qiuruihao@gmail.com>
2018-05-04 00:01:13 +0200

    clean code

---
+++ tools/plot.py
@@ -0,0 +1,28 @@
+## Model and visulization
+
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('ggplot')
+
+def plot_class(clf):
+    X_train, y_train = make_blobs(n_samples=200, centers=2,
+                  random_state=2, cluster_std=2.50)
+    
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    ax.set_xlabel('feature 1', color='gray')
+    ax.set_ylabel('feature 2', color='gray')
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, s=100, cmap='Paired', zorder=3)
+
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()        
+    x = np.linspace(xlim[0], xlim[1], 50)
+    y = np.linspace(ylim[0], ylim[1], 50)
+    yy, xx = np.meshgrid(y, x)
+    X_test = np.vstack([xx.ravel(), yy.ravel()]).T
+
+    clf.fit(X_train, y_train)
+    zz = clf.predict(X_test)
+    zz = zz.reshape(xx.shape)
+    ax.contourf(xx, yy, zz, cmap='Paired', alpha=0.4, zorder=1)
+    plt.show()
